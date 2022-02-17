6108b54b43bb149a9d1b1ac82faf7598b7d49fde
Oleksandr <box4reg17@gmail.com>
2022-02-17 23:46:40 +0200

    added hw13

--- HomeWork_lesson_13/hw13_PySpark.py
+++ HomeWork_lesson_13/hw13_PySpark.py
@@ -84,22 +84,65 @@ def task_3():
 
     get_top_film_category_df.show()
     
+def task_4():
+    """
+        вывести названия фильмов, которых нет в inventory.
+    """
+    film_df = spark.read.jdbc(url=pg_url, table='film', properties=pg_properties)
+    inventory_df = spark.read.jdbc(url=pg_url, table='inventory', properties=pg_properties)
+
+    filtered_films_df = film_df\
+        .join(inventory_df, inventory_df.film_id == film_df.film_id,'left')\
+        .select(film_df.title.alias('Films'))\
+        .where(inventory_df.inventory_id.isNull())\
+        .distinct().orderBy('Films')
+
+    filtered_films_df.show(1000,100)
+
+def task_5():
+    """
+        вывести топ 3 актеров, которые больше всего появлялись в фильмах в категории “Children”.
+        Если у нескольких актеров одинаковое кол-во фильмов, вывести всех..
+    """
+    pass
 
 
-# вывести категорию фильмов, на которую потратили больше всего денег.
-# вывести названия фильмов, которых нет в inventory.
-# вывести топ 3 актеров, которые больше всего появлялись в фильмах в категории “Children”. Если у нескольких актеров одинаковое кол-во фильмов, вывести всех..
-# вывести города с количеством активных и неактивных клиентов (активный — customer.active = 1). Отсортировать по количеству неактивных клиентов по убыванию.
-# вывести категорию фильмов, у которой самое большое кол-во часов суммарной аренды в городах (customer.address_id в этом city), и которые начинаются на букву “a”. То же самое сделать для городов в которых есть символ “-”.
+def task_6():
+    """
+        вывести города с количеством активных и неактивных клиентов (активный — customer.active = 1).
+        Отсортировать по количеству неактивных клиентов по убыванию.
+    """
+    customer_df = spark.read.jdbc(url=pg_url, table='customer', properties=pg_properties)
+    address_df = spark.read.jdbc(url=pg_url, table='address', properties=pg_properties)
+    city_df = spark.read.jdbc(url=pg_url, table='city', properties=pg_properties)
+
+    cities_user_stat_df = customer_df\
+        .join(address_df, customer_df.address_id == address_df.address_id, 'inner')\
+        .join(city_df, city_df.city_id == address_df.city_id , 'inner')\
+        .select(city_df.city.alias('City'), customer_df.active)\
+        .groupBy('City', 'active')\
+        .count()\
+        .sort('active', 'City', F.desc('count'))
+    
+    cities_user_stat_df.show(1000, 100)
+
+
+def task_7():
+    """
+        вывести категорию фильмов, у которой самое большое кол-во часов суммарной аренды в 
+        городах (customer.address_id в этом city), и которые начинаются на букву “a”.
+        То же самое сделать для городов в которых есть символ “-”.
+    """
+    pass
 
 
 
 if __name__ == '__main__':
-    #task_1()
+    task_1()
     #task_2()
-    task_3()
+    #task_3()
+    #task_4()
+    #task_5()
+    #task_6()
+    #task_7()
 
-
-
-
-    
